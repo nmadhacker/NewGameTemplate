b68403a93d683e13c4318779bd97fe2bdda3cc14
user <user@wificlient-10-200-34-213.uc.cl>
2017-01-21 20:08:48 -0300

    updating assets

---
+++ Assets/Scripts/ExecuteOnContact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExecuteOnContact : MonoBehaviour 
+{
+	[SerializeField] UnityEvent eventToTrigger;
+	[SerializeField] string tagtoCheck;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag.Equals (tagtoCheck)) 
+		{
+			eventToTrigger.Invoke ();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == tagtoCheck) 
+		{
+			eventToTrigger.Invoke ();
+		}
+	}
+}
